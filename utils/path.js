7503f62abfc75c59c4aac8264d6c085aebe9d7ec
DemonMartin <skubiduja1981@gmail.com>
2024-04-23 16:56:29 +0200

    Changed to default to the xgo releases of tlsClient and updated to use 1.7.4

--- utils/path.js
+++ utils/path.js
@@ -8,8 +8,8 @@ class TlsDependency {
     constructor() {
         this.arch = os.arch();
         this.platform = os.platform();
-        this.version = "1.7.2";
-        this.filename = "";
+        this.version = "1.7.4";
+        this.filename = "tls-client-xgo";
         this.extension = "";
         this.distribution = "";
         this.setDetails();
@@ -17,63 +17,39 @@ class TlsDependency {
 
     setDetails() {
         if (this.platform === "win32") {
-            this.filename = "tls-client-windows";
             this.extension = "dll";
-            this.distribution = this.arch.includes("64") ? "64" : "32";
+            this.distribution = this.arch.includes("64") ? "windows-amd64" : "windows-386";
         } else if (this.platform === "darwin") {
-            this.filename = "tls-client-darwin";
             this.extension = "dylib";
-            this.distribution = this.arch == "arm64" ? this.arch : "amd64";
+            this.distribution = this.arch == "arm64" ? "darwin-arm64" : "darwin-amd64";
         } else if (this.platform === "linux") {
-            this.filename = "tls-client-linux";
             this.extension = "so";
-            this.setLinuxDistribution();
+
+            const archMap = {
+                'arm64': 'linux-arm64',
+                'x64': 'linux-amd64',
+                'ia32': 'linux-386',
+                'arm': 'linux-arm-7', // assuming ARMv7
+                'ppc64': 'linux-ppc64le',
+                'riscv64': 'linux-riscv64',
+                's390x': 'linux-s390x',
+            };
+
+            const distribution = archMap[this.arch];
+
+            if (!distribution) {
+                console.error(`Unsupported architecture: ${this.arch}, defaulting to linux-amd64`);
+            }
+
+            this.distribution = distribution || 'linux-amd64';
         } else {
             console.error(`Unsupported platform: ${this.platform}`);
             process.exit(1);
         }
     }
 
-
-
-    setLinuxDistribution() {
-        let osType = os.type().toLowerCase();
-
-        if (osType.includes("linux")) {
-            let distro;
-            try {
-                distro = execSync('lsb_release -a').toString();
-            } catch (error) {
-                console.log("Unable to determine Linux distribution. Defaulting to specific distribution.");
-                this.distribution = this.arch == "arm64" ? this.arch : "armv7";
-                return;
-            }
-
-            if (distro.toLowerCase().includes("ubuntu")) {
-                this.distribution = "ubuntu-amd64";
-            } else if (distro.toLowerCase().includes("alpine")) {
-                this.distribution = `alpine-amd64`;
-            } else {
-                this.distribution = this.arch == "arm64" ? this.arch : "armv7";
-            }
-        } else {
-            console.log("Unsupported OS type. Defaulting to specific distribution.");
-            this.distribution = this.arch == "arm64" ? this.arch : "armv7";
-        }
-    }
-
-    /**
-     * @typedef {Object} TlsDependencyPath
-     * @property {string} DOWNLOAD_PATH - The download path of the TLS library
-     * @property {string} TLS_LIB_PATH - The destination path of the TLS library
-     */
-
-    /**
-     * @description Get the TLS dependency path and download url based on the platform 
-     * @returns {TlsDependencyPath} An object with the download path and the destination path
-     */
     getTLSDependencyPath(customPath = null) {
-        let _filename = `${this.filename}-${this.distribution}-v${this.version}.${this.extension}`;
+        let _filename = `${this.filename}-${this.version}-${this.distribution}.${this.extension}`;
         const url = new URL(`https://github.com/bogdanfinn/tls-client/releases/download/v${this.version}/${_filename}`);
         const downloadFolder = customPath ?? (os.tmpdir() ?? process.cwd());
         if (!fs.existsSync(downloadFolder))
